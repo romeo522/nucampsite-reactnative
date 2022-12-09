f7addadd2bbb3c733d412daf239a7f6b923b4ec8
romeo <romeorangeljr@gmail.com>
2022-12-09 13:18:36 -0600

    ReactNative | Week 1 | Components Part 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,10 +1,26 @@
 import { useState } from "react";
 import { CAMPSITES } from "../shared/campsites";
 import DirectoryScreen from "./DirectoryScreen";
+import { View } from "react-native";
+import CampsiteInfoScreen from "./CampsiteInfoScreen";
 
 const Main = () => {
   const [campsites, setCampsites] = useState(CAMPSITES);
-  return <DirectoryScreen campsites={campsites} />;
+  const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+
+  return (
+    <View style={{ flex: 1 }}>
+      <DirectoryScreen
+        campsites={campsites}
+        onPress={(campsiteId) => setSelectedCampsiteId(campsiteId)}
+      />
+      <CampsiteInfoScreen
+        campsite={
+          campsites.filter((campsite) => campsite.id === selectedCampsiteId)[0]
+        }
+      />
+    </View>
+  );
 };
 
 export default Main;
