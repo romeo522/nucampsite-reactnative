087a84c90025e4c085d141e40563da482da25887
romeo <romeorangeljr@gmail.com>
2022-12-09 12:03:52 -0600

    ReactNative | Week 1 | Components Part 1

---
+++ screens/MainComponent.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { CAMPSITES } from "../shared/campsites";
+import DirectoryScreen from "./DirectoryScreen";
+
+const Main = () => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+  return <DirectoryScreen campsites={campsites} />;
+};
+
+export default Main;
