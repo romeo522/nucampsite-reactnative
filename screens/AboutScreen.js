dee931797f60d211ffbaf8dff239f4799581cff7
romeo <romeorangeljr@gmail.com>
2022-12-10 12:24:26 -0600

    ReactNative | Week 1 | Workshop Task 1

---
+++ screens/AboutScreen.js
@@ -0,0 +1,7 @@
+import { ScrollView } from "react-native";
+
+const AboutScreen = () => {
+  return <ScrollView></ScrollView>;
+};
+
+export default AboutScreen;
---
+++ screens/ContactScreen.js
@@ -0,0 +1,7 @@
+import { ScrollView } from "react-native";
+
+const ContactScreen = () => {
+  return <ScrollView></ScrollView>;
+};
+
+export default ContactScreen;
